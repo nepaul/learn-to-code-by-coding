23c90fa0467851c2bf9c09acb256a2e4377a70dd
Poe Nee <nibo@MacBook-Pro.local>
2016-11-11 19:55:03 +0800

    remove react-redux-jwt

--- react-redux-jwt/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/koa/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react-redux/index.d.ts" />
-/// <reference path="globals/react-router/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
