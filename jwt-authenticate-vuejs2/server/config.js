7ac6fdc6f4ba4622920894a2855c6571aedaa0bb
nepaul <xs.nepaul@gmail.com>
2017-03-12 21:49:52 +0800

    Add mysql connections with sequelize

--- jwt-authenticate-vuejs2/server/config.js
+++ jwt-authenticate-vuejs2/server/config.js
@@ -1,4 +1,24 @@
-exports = {
+const path = require('path');
+
+
+module.exports = {
   port: process.env.PORT || 3000,
   APIPrefix: '/api/v1',
+  DB: {
+    db: 'jwtVue2',
+    user: 'root',
+    password: '',
+    options: {
+      host: 'localhost',
+      dialect: 'mysql',
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+      // SQLite only
+      // storage: path.normalize(`${__dirname}/sqlite.db`),
+    },
+  }
+
 }
