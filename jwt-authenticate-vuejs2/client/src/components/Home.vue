f004535e4fd124d77afb3b42c4651b9152731acc
nepaul <xs.nepaul@gmail.com>
2017-03-19 23:18:31 +0800

    Add jwt-auth-jwt Home

---
+++ jwt-authenticate-vuejs2/client/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-link to="login" class="el-button">Log In</router-link>
+    <router-link class="el-button" type="primary" to="signup">Create Account</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- jwt-authenticate-vuejs2/client/src/router/index.js
+++ jwt-authenticate-vuejs2/client/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Home from '@/components/Home';
+import Login from '@/components/Login';
+import Signup from '@/components/Signup';
+import Dashboard from '@/components/Dashboard';
 
 Vue.use(Router);
 
@@ -8,8 +11,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
     },
   ],
 });
