4064d29b9d1507a75b136ffb9c2d08231a5048ed
nepaul <xs.nepaul@gmail.com>
2017-03-20 21:52:39 +0800

    Use axios to replace vue-resource

--- jwt-authenticate-vuejs2/client/src/main.js
+++ jwt-authenticate-vuejs2/client/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
 import App from './App';
@@ -8,6 +9,9 @@ import router from './router';
 
 
 Vue.use(ElementUI);
+
+Vue.prototype.$http = axios;
+Vue.axios = axios;
 
 Vue.config.productionTip = false;
 
